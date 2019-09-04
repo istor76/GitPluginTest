bb51d0801dcb84a6dc65e078c1204aaf2fe03a6f
Ismayl Rahmaoui <ismayl.rahmaoui@gmail.com>
2019-09-04 12:37:57 -0400

    Fixing the bug number 1234.

--- GITTestProject/src/ismayl/app/GitTest.java
+++ GITTestProject/src/ismayl/app/GitTest.java
@@ -3,7 +3,7 @@ package ismayl.app;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hello Git, from branch bug#1234");
 	}
 
 }
